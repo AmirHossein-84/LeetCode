6f1577831ec34d25a4944d97189f90df5c943e2c
Amirhossein <130301283+AmirHossein-84@users.noreply.github.com>
2025-11-11 15:20:16 +0330

    Time: 9 ms (45.05%), Space: 23 MB (19.63%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        temp = ''.join(c.lower() for c in s if c.isalnum())
+        temp = temp.replace(" ", "")
+        temp_list = [i for i in temp if True]
+
+        return True if temp_list == temp_list[-1: : -1] else False
+        
